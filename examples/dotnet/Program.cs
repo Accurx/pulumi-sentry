d3adc606f967b409bb643c3ef4143c09735f6f95
Baskar Natarajan <baskar.natarajan@accurx.com>
2022-09-28 14:34:01 +0100

    A sample dotnet project using the provider

---
+++ examples/dotnet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+using Pulumi.Sentry;
+
+namespace Sentry
+{
+   class Sentry : Stack
+   {
+      static Task<int> Main() => Deployment.RunAsync<Sentry>();
+      
+      [Output] public Output<string> ClientKeyDsn { get; set; }
+      
+      public Sentry()
+      {
+         var project = new Project("test-project", new ProjectArgs
+         {
+            Organization = "test-org",
+            Team = "test-team",
+            Platform = "dotnet",
+         });
+
+         var clientKey = new Key("test-key", new KeyArgs
+         {
+            Organization = "test-org",
+            Project = project.Name
+         },
+            new CustomResourceOptions{ AdditionalSecretOutputs = new List<string>{"dsnPublic"}});
+
+         ClientKeyDsn = clientKey.DsnPublic;
+      }
+   }
+}
---
+++ examples/dotnet/README.md
@@ -0,0 +1,3 @@
+Add the Sentry authentication token as a pulumi secret using:
+
+`pulumi config set sentry:token <GENERATED API TOKEN FROM SENTRY> --secret`
